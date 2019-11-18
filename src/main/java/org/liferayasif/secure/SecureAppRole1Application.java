2dca3f029c7e5bfa9a0df55192348eb6bdc46929
Mohammad Asif Fatab Alam <fatabmo@dc1.manulife.net.my>
2019-11-18 17:46:27 +0800

    First Commit of Spring Boot Spring Security

---
+++ src/main/java/org/liferayasif/secure/SecureAppRole1Application.java
@@ -0,0 +1,13 @@
+package org.liferayasif.secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAppRole1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAppRole1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/liferayasif/secure/SecureAppRole1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liferayasif.secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAppRole1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
